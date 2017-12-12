b1963b43c5090b000ea323f11cd09907e9e5a84b
okkah <trombonehide@yahoo.co.jp>
2017-12-12 17:05:39 +0900

    add pyenv

--- config.fish
+++ config.fish
@@ -2,3 +2,13 @@ function fish_user_key_bindings
     bind \cr 'peco_select_history (commandline -b)'
     bind \c] peco_select_ghq_repository
 end
+
+#pyenv
+set -gx PYENV_ROOT "$HOME/.pyenv"
+set -x PATH $PATH "$PYENV_ROOT/bin"
+status --is-interactive; and . (pyenv init - | psub)
+
+# tmux
+function tmr
+    tmux new -s $argv[1]; or tmux attach -d -t $argv[1]
+end
